eb1d33915bd7dc6fb201574057bd3a5f99fee71e
Mikael Nilsson <mikael.nilsson@devisesthlm.se>
2021-01-09 15:17:49 +0100

    Added support for multiple shelters

--- main.py
+++ main.py
@@ -15,14 +15,33 @@ import dogcsv
 
 chromePath = "chromedriver_win32\\chromedriver.exe"
 # chromePath = "chromedriver_linux64/chromedriver"
-hundUrls = [
-    "https://hundarutanhem.se/hundarna/mellanstora-hundar/",
-    "https://hundarutanhem.se/hundarna/stora-hundar/"
-
+shelters = [
+    # {   "name": "hundarutanhem",
+    #     "url": [
+    #         "https://hundarutanhem.se/hundarna/mellanstora-hundar/",
+    #         "https://hundarutanhem.se/hundarna/stora-hundar/"
+    #     ],
+    #     "element_tree": {
+    #         "classes": "polaroid", "location": "img", "source": "src"
+    #     },
+    #     "outfile": "hundarutanhem.csv"
+    # },
+    {
+        "name": "hundstallet",
+        "url": ["https://hundstallet.se/soker-hem/"],
+        "element_tree": {
+            "classes": "small-12 medium-6 large-4 cell", "location": "img", "source": "src"
+        },
+        "outfile": "hundstallet.csv"
+    }
 ]
-outPath = "hundar/" + date.today().strftime("%Y%m%d")
 
-dogfile = 'dogs.csv'
+
+# dogfile = 'dogs.csv'
+
+## returns folder path
+def outPath(shelter):
+    return "hundar/" + shelter["name"] + "/" + date.today().strftime("%Y%m%d")
 
 
 ## downloads content of chosen url
@@ -73,35 +92,49 @@ def get_filename_from_url(url):
 
 
 # Saves all images from site on url
-## TODO: This one will need to be updated for other dog shelter
-def get_images_from_url(url):
-    content = get_content_from_url(url)
-    image_urls = parse_image_urls(
-        content=content, classes="polaroid", location="img", source="src",
-    )
-    save_urls_to_csv(image_urls)
-    save_names_to_csv(image_urls)
+def save_images_from_shelter(shelter):
 
-    if not os.path.isdir(outPath):
-        os.mkdir(outPath)
+    image_urls = []
+
+    outFolder = outPath(shelter)
+
+    if not os.path.isdir(outFolder):
+        os.mkdir(outFolder)
+
+    ## TODO: This block will need to be updated for other dog shelters
+    for url in shelter["url"]:
+        print("collecting dogs")
+        print(url)
+        content = get_content_from_url(url)
+        url_list = parse_image_urls(
+            content=content, classes=shelter["element_tree"]["classes"], location=shelter["element_tree"]["location"], source=shelter["element_tree"]["source"]
+        )
+        image_urls.extend(url_list)
+
+    save_urls_to_csv(image_urls)
+    save_names_to_csv(image_urls, shelter["outfile"])
 
     for image_url in image_urls:
         print(image_url)
         get_filename_from_url(image_url)
         get_and_save_image_to_file(
-            image_url, output_dir=pathlib.Path(outPath),
+            image_url, output_dir=pathlib.Path(outFolder),
         )
 
 
 ## adds current list of dog names to csv file
+def save_names_to_csv(image_urls, dogfile):
     dogs, fields=dogcsv.readCsv(dogfile, ',')
 
     dogNames = []
     for url in image_urls:
         dogNames.append(dogname_from_url(url))
+
+    today = date.today().strftime("%Y%m%d")
         
-    dogs = dogcsv.addDate(dogs, dogNames, date.today().strftime("%Y%m%d"))
-    dogcsv.writeCsv(dogs, dogfile, ',')
+    dogs = dogcsv.addDate(dogs, dogNames, today)
+    fields.append(today)
+    dogcsv.writeCsv(dogs, fields, dogfile, ',')
 
 
 ## extracts dog name from image url
@@ -129,8 +162,8 @@ def dogname_from_url(stringToSearch):
 
 
 def main():
-    for url in hundUrls:
-        get_images_from_url(url)
+    for shelter in shelters:
+        save_images_from_shelter(shelter)
 
 
 
